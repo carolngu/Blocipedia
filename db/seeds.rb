2a486509dd91c99206860106abcf0b0c2e63844b
Carol <carolngu@gmail.com>
2016-04-13 21:05:39 -0700

    User Story 6 Seeding Data: Completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,8 @@
-require 'random_data'
-
 5.times do
   User.create!(
-  name: RandomData.random_name,
-  email: RandomData.random_email,
-  password: RandomData.random_sentence
+  name: Faker::Name.name,
+  email: Faker::Internet.email,
+  password: Faker::Internet.password(8)
   )
 end
 users= User.all
@@ -12,8 +10,8 @@ users= User.all
 50.times do
   wiki = Wiki.create!(
     user: users.sample,
-    title: RandomData.random_sentence,
-    body: RandomData.random_paragraph,
+    title: Faker::Lorem.sentence,
+    body: Faker::Lorem.paragraph,
     private: false
   )
 end
